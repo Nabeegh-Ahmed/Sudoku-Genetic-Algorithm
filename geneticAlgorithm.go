ee4026bca4386b0cc08d44d0a16b7511403c1f42
Nabeegh Ahmed <nabeegh08@gmail.com>
2022-04-07 09:53:05 +0500

    down to 2

--- geneticAlgorithm.go
+++ geneticAlgorithm.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 )
 
-var PopulationSize = 10
+var PopulationSize = 7000
+var Outliers = 0.1
+var CrossOvers = 0.6
+var Mutants = 0.4
 
 func randBetween(a int, b int) int {
 	span := (b - a) + 1
@@ -28,7 +32,7 @@ func createIndividual() Individual {
 		for j := 0; j < 9; j++ {
 			status, value := isFromInitialState(i, j)
 			if !status {
-				newIndividual.gene[i][j] = randBetween(0, 9)
+				newIndividual.gene[i][j] = randBetween(1, 9)
 			} else {
 				newIndividual.gene[i][j] = value
 			}
@@ -37,7 +41,7 @@ func createIndividual() Individual {
 	return newIndividual
 }
 
-func sortPopulation(individuals []Individual) {
+func sortPopulation(individuals []Individual) []Individual {
 	for i := 1; i < PopulationSize; i++ {
 		key := individuals[i]
 		j := i - 1
@@ -47,6 +51,7 @@ func sortPopulation(individuals []Individual) {
 		}
 		individuals[j+1] = key
 	}
+	return individuals
 }
 
 func generatePopulation() []Individual {
@@ -57,4 +62,109 @@ func generatePopulation() []Individual {
 		population = append(population, individual)
 	}
 	return population
+}
+
+func mutate(individual Individual) Individual {
+	mutant := Individual{}
+	mutant.gene = individual.gene
+
+	_, mistakes := checkBoard(&individual.gene)
+
+	if mistakes < 5 {
+		for i := 0; i < randBetween(0, 20); {
+			randomX := randBetween(0, 8)
+			randomY := randBetween(0, 8)
+
+			status, _ := isFromInitialState(randomX, randomY)
+			if !status {
+				i++
+				mutant.gene[randomX][randomY] = randBetween(1, 9)
+			}
+		}
+		mutant.fitness = fitnessFunction(&mutant.gene)
+		return mutant
+	}
+
+	process := randBetween(0, 3)
+	if process == 0 {
+		// mutate a random row
+		randomRow := randBetween(0, 8)
+		for i := 0; i < 9; i++ {
+			status, _ := isFromInitialState(randomRow, i)
+			if !status {
+				mutant.gene[randomRow][i] = randBetween(1, 9)
+			}
+		}
+	} else if process == 1 {
+		// mutate a random column
+		randomCol := randBetween(0, 8)
+		for i := 0; i < 9; i++ {
+			status, _ := isFromInitialState(i, randomCol)
+			if !status {
+				mutant.gene[i][randomCol] = randBetween(1, 9)
+			}
+		}
+	} else {
+		// mutate a random grid
+		randomGridX := randBetween(0, 2)
+		randomGridY := randBetween(0, 2)
+
+		for i := randomGridX * 3; i < randomGridX*3+3; i++ {
+			for j := randomGridY * 3; j < randomGridY*3+3; j++ {
+				status, _ := isFromInitialState(i, j)
+				if !status {
+					mutant.gene[i][j] = randBetween(1, 9)
+				}
+			}
+		}
+	}
+	mutant.fitness = fitnessFunction(&mutant.gene)
+	return mutant
+}
+
+func naturalSelection(individuals []Individual) []Individual {
+	var buffer []Individual
+	outliersRange := int(float64(PopulationSize) * Outliers)
+	crossOverRange := int(float64(PopulationSize) * CrossOvers)
+
+	for i := 0; i < outliersRange; i++ {
+		buffer = append(buffer, individuals[i])
+	}
+	for i := outliersRange; i < crossOverRange+outliersRange; i++ {
+		parent1 := individuals[randBetween(0, PopulationSize-1)]
+		parent2 := individuals[randBetween(0, PopulationSize-1)]
+
+		var offspring1 Individual
+
+		barrier := randBetween(1, 9)
+		for i := 0; i < barrier; i++ {
+			offspring1.gene[i] = parent2.gene[i]
+		}
+		for i := barrier; i < 9; i++ {
+			offspring1.gene[i] = parent1.gene[i]
+		}
+
+		offspring1.fitness = fitnessFunction(&offspring1.gene)
+
+		buffer = append(buffer, offspring1)
+	}
+	for i := outliersRange + crossOverRange; i < PopulationSize; i++ {
+		buffer = append(buffer, mutate(individuals[randBetween(0, PopulationSize-1)]))
+	}
+	return buffer
+}
+
+func geneticAlgorithm() {
+	population := generatePopulation()
+	for i := 0; i < 100000; i++ {
+		population = sortPopulation(population)
+		fmt.Println(population[0].fitness, " ", i)
+		// printBoard(population[0].gene)
+		if population[0].fitness == 0 {
+			printBoard(population[0].gene)
+			break
+		}
+		population = naturalSelection(population)
+	}
+	printBoard(population[0].gene)
 }
--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"math/rand"
+	"time"
+)
 
 func main() {
 	var board [9][9]int
+	rand.Seed(time.Now().UnixNano())
+
 	initBoard(&board)
 	var gameInitialState []Pair
-	gameInitialState = append(gameInitialState, Pair{0, 1})
+	gameInitialState = append(gameInitialState,
+		Pair{0, 1}, Pair{0, 7},
+		Pair{1, 1}, Pair{1, 7},
+		Pair{2, 0}, Pair{2, 8},
+		Pair{3, 2}, Pair{3, 3}, Pair{3, 5}, Pair{3, 6},
+		Pair{4, 3}, Pair{4, 4}, Pair{4, 6}, Pair{4, 7},
+		Pair{5, 3}, Pair{5, 4}, Pair{5, 6}, Pair{5, 7},
+		Pair{6, 4},
+		Pair{7, 0}, Pair{7, 1}, Pair{7, 7}, Pair{7, 8},
+		Pair{8, 1}, Pair{8, 3}, Pair{8, 5}, Pair{8, 7})
 	var values []int
-	values = append(values, 9)
+	values = append(values, 1, 6, 2, 3, 9, 1, 2, 8, 6, 4, 6, 9, 2, 3, 4, 3, 7, 1, 8, 7, 3, 5, 8, 8, 2, 1, 9)
 	setInitialState(&board, gameInitialState, values)
-	printBoard(board)
 
-	population := generatePopulation()
-	sortPopulation(population)
-
-	for i := 0; i < PopulationSize; i++ {
-		fmt.Println(population[i].fitness)
-	}
+	geneticAlgorithm()
 }
